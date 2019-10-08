ae3d74a35690eca39a39b7009eaf515efb027f1b
Jim Parry <jim_parry@telus.net>
2019-10-07 19:14:23 -0700

    Fix model & images

--- app/Models/Places.php
+++ app/Models/Places.php
@@ -10,35 +10,35 @@ class Places
 
 	// mock data: an array of records (also arrays)
 	protected $data = [
-		[
+		'1'	 => [
 			'id'			 => 1,
 			'name'			 => 'Hong Kong',
 			'description'	 => 'A special administrative region on the eastern side of the Pearl River estuary in southern China.',
 			'link'			 => 'http://www.discoverhongkong.com/eng/index.jsp',
 			'image'			 => 'hongkong.jpg',
 		],
-		[
+		'2'	 => [
 			'id'			 => 2,
 			'name'			 => 'Macau',
 			'description'	 => 'A special administrative region on the western side of the Pearl River estuary in southern China.',
 			'link'			 => 'http://en.macaotourism.gov.mo/index.php',
 			'image'			 => 'macau.jpg',
 		],
-		[
+		'3'	 => [
 			'id'			 => 3,
 			'name'			 => 'Thailand',
 			'description'	 => 'A country at the centre of the Southeast Asian Indochinese peninsula composed of 76 provinces.',
 			'link'			 => 'http://www.tourismthailand.org/',
 			'image'			 => 'thailand.jpg',
 		],
-		[
+		'4'	 => [
 			'id'			 => 4,
 			'name'			 => 'Japan',
 			'description'	 => 'An island country in East Asia.',
 			'link'			 => 'http://www.jnto.go.jp/eng/',
 			'image'			 => 'japan.jpg',
 		],
-		[
+		'5'	 => [
 			'id'			 => 5,
 			'name'			 => 'Vietnam',
 			'description'	 => 'The easternmost country on the Indochina Peninsula.',
@@ -55,7 +55,7 @@ class Places
 	public function find($id = null)
 	{
 		if ( ! empty($id) && isset($this->data[$id]))
-			return $data[$id];
+			return $this->data[$id];
 		return null;
 	}
 
