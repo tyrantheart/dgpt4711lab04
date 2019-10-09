700a743dd3b858c83ae4e2ba86159c4ff4881719
tyrantheart <1226950749@qq.com>
2019-10-09 09:55:40 +0800

    lab4 done

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -69,7 +69,6 @@ $routes->setAutoRoute(true);
  * Route Definitions
  * --------------------------------------------------------------------
  */
-
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
